52e60b82c87cc23cfb3896fb63c8a00e014bdedd
sujeong lim <20180998@sungshin.ac.kr>
2022-02-10 10:05:22 +0900

    1Week: 예산 개선 문제풀이 추가 (Swift)

--- sujeong/예산.swift
+++ sujeong/예산.swift
@@ -1,2 +1,30 @@
 import Foundation
 
+func solution1(_ d:[Int], _ budget:Int) -> Int {
+    var sumOfCost: Int = 0
+    var deptCnt: Int = 0
+    let costs = d.sorted()
+    
+    for cost in costs {
+        let preSumOfCost: Int = sumOfCost + cost
+        if preSumOfCost <= budget {
+            sumOfCost = preSumOfCost
+            deptCnt += 1
+        } else { break }
+    }
+    
+    return deptCnt
+}
+
+func solution2(_ d:[Int], _ budget:Int) -> Int {
+    var varBudget: Int = budget
+    var deptCnt: Int = 0
+    let costs: [Int] = d.sorted()
+    
+    while deptCnt < d.count && varBudget - costs[deptCnt] >= 0 {
+        varBudget -= costs[deptCnt]
+        deptCnt += 1
+    }
+    
+    return deptCnt
+}
