1ad5c09dca4ccd9997d56b60bebd11c9ed113016
Seungmi kim <46683113+imim-seung@users.noreply.github.com>
2022-02-12 10:51:43 +0900

    Delete 예산.py

--- seungmi/예산.py
+++
@@ -1,9 +0,0 @@
-def solution(d, budget):
-    d.sort()
-    d_number=0
-    for request in d:
-        if budget>=request:
-            budget-=request
-            d_number+=1
-        
-    return d_number
