3841367105f7fb495c7a44261cab603f82a25119
Seungmi kim <46683113+imim-seung@users.noreply.github.com>
2022-02-19 23:20:45 +0900

    Revert "[week2] seungmi"

--- seungmi/week2/문자열 내 마음대로 정렬하기.py
+++
@@ -1,6 +0,0 @@
-def solution(strings, n):
-    nth_letters=[]
-    for word in strings:
-        nth_letters.append((word[n],word))
-    nth_letters=sorted(nth_letters)
-    return [i[1] for i in nth_letters]
--- seungmi/week2/하샤드 수.py
+++
@@ -1,2 +0,0 @@
-def harshad(n):
-  return n % sum([int(c) for c in str(n)]) == 0
