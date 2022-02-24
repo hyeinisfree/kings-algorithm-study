74fbd585a51f85463ca101408456c10903196bf6
chaewss <chaewseo12@gmail.com>
2022-02-25 01:22:38 +0900

    [week3] 괄호 회전하기

---
+++ chaewon/week3/괄호회전하기.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+def solution(ip):
+    def check(s):
+        stack = []
+        for c in s:
+            if c == '(' or c == '{' or c == '[':
+                stack.append(c)
+            else:
+                if not stack:
+                    return False
+                x = stack.pop()
+                if c == ')' and x != '(':
+                    return False
+                elif c == ')' and x != '(':
+                    return False
+                elif c == '}' and x != '{':
+                    return False
+        return len(stack) == 0
+
+    ip = deque(ip)
+    answer = 0
+    for x in range(len(ip)):
+        ip.rotate(-1)
+        if check(ip):
+            answer += 1
+    return answer
