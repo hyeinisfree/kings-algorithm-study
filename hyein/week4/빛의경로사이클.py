3c3bb35a3ec99339c5fa861ffad2d33d0af5b057
hyeinisfree <kimhyein7110@gmail.com>
2022-03-06 16:47:01 +0900

    [week4] 빛의 경로 사이클

--- hyein/week4/빛의경로사이클.py
+++ hyein/week4/빛의경로사이클.py
@@ -1,38 +1,73 @@
-dx = [1,0,-1,0]
-dy = [0,-1,0,1]
+dx = [-1,0,1,0] # 위, 오른쪽, 아래, 왼쪽
+dy = [0,1,0,-1]
 
-def solution(grid):
-    global visited,n,m
-    n = len(grid)
-    m = len(grid[0])
+# Recursion
+import sys
+sys.setrecursionlimit(10 ** 6)
+
+def solution1(grid):
+    global answer, visited
     answer = []
-    visited = [[[False]*4 for _ in range(m)] for _ in range(n)]
-    for sx in range(n):
-        for sy in range(m):
+    visited = [[[False]*4 for _ in range(len(grid[0]))] for _ in range(len(grid))]
+    
+    for x in range(len(grid)):
+        for y in range(len(grid[0])):
             for d in range(4):
-                if not visited[sx][sy][d]:
-                    rst = simul(sx,sy,d,grid)
-                    if rst != 0:
-                        answer.append(rst)
+                dfs([x,y,d], [x,y,d], 1, grid)
+                    
     return sorted(answer)
 
-def simul(sx,sy,sd,grid):
+def dfs(state, org, cnt, grid):
+    x, y, d = state[0], state[1], state[2]
+    visited[x][y][d] = True
+    
+    nx = (x + dx[d]) % len(grid)
+    ny = (y + dy[d]) % len(grid[0])
+    
+    if grid[nx][ny] == 'R':
+        d = (d+1)%4
+    elif grid[nx][ny] == 'L':
+        d = (d-1)%4
+    if (nx, ny, d) == (org[0], org[1], org[2]):
+        answer.append(cnt)
+        return
+    if not visited[nx][ny][d]:
+        dfs([nx, ny, d], org, cnt+1, grid)
+        
+# Iterative
+def solution2(grid):
     global visited
-    x,y,d=sx,sy,sd
-    cnt = 0
-    visited[sx][sy][sd] = True
-    while True:
-        x = (x + dx[d]) % n
-        y = (y + dy[d]) % m
-        cnt += 1
+    answer = []
+    visited = [[[False]*4 for _ in range(len(grid[0]))] for _ in range(len(grid))]
+    
+    for x in range(len(grid)):
+        for y in range(len(grid[0])):
+            for d in range(4):
+                if not visited[x][y][d]:
+                    cnt = dfs(x,y,d,grid)
+                    if cnt != 0:
+                        answer.append(cnt)
+                
+    return sorted(answer)
 
-        if grid[x][y] == 'R':
-            d = (d+1)%4
-        elif grid[x][y] == 'L':
-            d = (d-1)%4
-        if visited[x][y][d]:
-            if (x,y,d) == (sx,sy,sd):
+def dfs(x, y, d, grid):
+    visited[x][y][d] = True
+    nx, ny, nd = x, y, d``
+    cnt = 0
+    while True:
+        nx = (nx + dx[nd]) % len(grid)
+        ny = (ny + dy[nd]) % len(grid[0])
+        cnt += 1
+        
+        if grid[nx][ny] == 'R':
+            nd = (nd+1)%4
+        elif grid[nx][ny] == 'L':
+            nd = (nd-1)%4
+        if visited[nx][ny][nd]:
+            if (nx,ny,nd) == (x,y,d):
                 return cnt
             else:
                 return 0
-        visited[x][y][d] = True
+        visited[nx][ny][nd] = True
+
+print(solution2(["SL","LR"]))
