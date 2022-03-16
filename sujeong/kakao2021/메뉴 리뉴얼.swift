3dbc63e798beee3eedf08a072169aa2fe8250d13
sio2whocodes <20180998@sungshin.ac.kr>
2022-03-16 20:28:57 +0900

    [kakao2021] 순위 검색

--- sujeong/kakao2021/메뉴 리뉴얼.swift
+++ sujeong/kakao2021/메뉴 리뉴얼.swift
@@ -3,14 +3,14 @@ import Foundation
 var survey:[String:Int] = [String:Int]()
 
 func solution(_ orders:[String], _ course:[Int]) -> [String] {
-
-    let ordersArr:[[Character]] = orders.map{ Array($0).sorted() }
-
+    
     var result:[String] = []
-    for n in course {
+    let ordersArr:[[Character]] = orders.map{ Array($0).sorted() }
+    
+    for neededNumOfMenu in course {
         survey = [:]
         for order in ordersArr {
-            getCourse(-1, order, "", n)
+            getCourse(-1, order, "", neededNumOfMenu)
         }
         result += selectFinalCourse()
     }
@@ -18,14 +18,14 @@ func solution(_ orders:[String], _ course:[Int]) -> [String] {
     return result.sorted()
 }
 
-func getCourse(_ index:Int, _ order:[Character], _ course:String, _ n:Int) {
-    if course.count == n {
+func getCourse(_ index:Int, _ order:[Character], _ course:String, _ neededNumOfMenu:Int) {
+    if course.count == neededNumOfMenu {
         survey[course] = survey[course] == nil ? 1 : survey[course]! + 1
         return
     }
     
     for i in index+1..<order.count {
-        getCourse(i, order, "\(course)\(order[i])", n)
+        getCourse(i, order, "\(course)\(order[i])", neededNumOfMenu)
     }
 }
 
