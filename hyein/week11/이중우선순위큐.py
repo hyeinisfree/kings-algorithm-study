916bc8cff05829dd67375a1e261bb230f325fb43
hyeinisfree <kimhyein7110@gmail.com>
2022-04-30 17:31:41 +0900

    [week11] 이중우선순위큐

---
+++ hyein/week11/이중우선순위큐.py
@@ -0,0 +1,21 @@
+import heapq
+
+def solution(operations):
+    heap = []
+
+    for operation in operations:
+        operator, operand = operation.split()
+        operand = int(operand)
+
+        if operator == 'I':
+            heapq.heappush(heap, operand)
+        elif heap:
+            if operand < 0:
+                heapq.heappop(heap)
+            else:
+                heap.remove(max(heap))
+
+    if not heap:
+        return [0, 0]
+
+    return [max(heap), heap[0]]
